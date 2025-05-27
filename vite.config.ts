c06f10d49f337f72008bcdae30de57d2268260d8
Rafael Hurpia da Rocha <focusdive@rafaelhdr.com.br>
2025-05-27 10:00:44 +0200

    feat: add react sourcemap

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  build: {
+    sourcemap: true, // Source map generation must be turned on
+  },
   server: {
     host: "::",
     allowedHosts: ['focusdive.app', '867917a8-7dc5-4c33-8d1d-c97fe6b92624.lovableproject.com'],
@@ -14,6 +18,11 @@ export default defineConfig(({ mode }) => ({
     react(),
     mode === 'development' &&
     componentTagger(),
+    sentryVitePlugin({
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      org: "focus-dive",
+      project: "javascript-react",
+    }),
   ].filter(Boolean),
   resolve: {
     alias: {
