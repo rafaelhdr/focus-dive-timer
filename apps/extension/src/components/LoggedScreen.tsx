5ac226db68a5b341b0730e4c19a81727cbc32898
Rafael Hurpia da Rocha <git@rafaelhdr.com.br>
2026-01-16 13:33:50 +0100

    feat: extension loads the settings to the timer

--- apps/extension/src/components/LoggedScreen.tsx
+++ apps/extension/src/components/LoggedScreen.tsx
@@ -2,11 +2,13 @@ import { Dashboard } from "./Dashboard";
 import { useTimerElapsedDetector, useTimerRealtime } from "@focusdive/timer";
 import { useOnTimerFinished } from "@/hooks/useOnTimerFinished";
 import { useTimerFinishedAlarm } from "@/hooks/useTimerFinishedAlarm";
+import { useSettingsTimerBridge } from "@/hooks/useSettingsTimerBridge";
 
 function TimerEffects() {
   useTimerRealtime();
   useTimerElapsedDetector();
   useOnTimerFinished();
+  useSettingsTimerBridge();
   useTimerFinishedAlarm();
   return null;
 }
