a543f80cb20f612443d55fdf08595f719b192932
Rafael Hurpia da Rocha <git@rafaelhdr.com.br>
2026-02-12 08:48:43 +0100

    fix: null token being sent

--- packages/settings/src/services/settings.ts
+++ packages/settings/src/services/settings.ts
@@ -7,7 +7,7 @@ async function authHeaders() {
   const accessToken = await getAccessToken();
   return {
     "Content-Type": "application/json",
-    Authorization: `Bearer ${accessToken}`,
+    ...(accessToken && { Authorization: `Bearer ${accessToken}` }),
   };
 }
 
